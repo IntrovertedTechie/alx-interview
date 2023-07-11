508de78422274957efdf9c113b2639e7a31fc67e
introvertedtechie <zillionaire234@gmail.com>
2023-07-11 08:08:24 +0000

    minimum ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n = n // divisor
+        else:
+            divisor += 1
+
+    return operations
