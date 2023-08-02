c325a7f04f55aff2742d11d619a9590e32aa8913
Adio Adebayo <zillionaire234@gmail.com>
2023-08-02 10:38:46 +0100

    Add files via upload

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" N Queens with Backtracking"""
+import sys
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    
+    # Check if there is a queen in the upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    # Check if there is a queen in the upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+    
+    return True
+
+def nqueens(board, row, N):
+    if row == N:
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    print(f'[{i}, {j}]', end=' ')
+        print()
+        return
+    
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            nqueens(board, row + 1, N)
+            board[row][col] = 0
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+        if N < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    nqueens(board, 0, N)
+
+if __name__ == "__main__":
+    main()
