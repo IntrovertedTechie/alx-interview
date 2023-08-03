5efa6716526f98fca0d0a1542e880094ada400f1
Adio Adebayo <zillionaire234@gmail.com>
2023-08-03 07:29:19 +0100

    Add files via upload

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""
+This code solves the N queens problems 
+"""
+
+
+def nqueens(n):
+    """
+    This function solves the N queens problem 
+    """
+
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
+
+    board = [[0 for i in range(n)] for j in range(n)]
+    solutions = []
+    solve(board, 0, solutions)
+    return solutions
+
+
+def solve(board, row, solutions):
+    """
+    This function recursively solves the N queens problems 
+    """
+
+    if row == n:
+        solutions.append(board[:])
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row][col] = 1
+            solve(board, row + 1, solutions)
+            board[row][col] = 0
+
+def is_safe(board, row, col):
+    """
+    This function checks if a queen can be placed in a given row or column
+    """
+
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    for i in range(row):
+        for j in range(n):
+            if board[i][j] == 1 and col - j == row - i or col + j == row - i:
+                return False
+
+    return True
+
+if __name__ == "__main__":
+    n = int(input("Enter N: "))
+    solutions = nqueens(n)
+    for solution in solutions:
+        print(solution)
